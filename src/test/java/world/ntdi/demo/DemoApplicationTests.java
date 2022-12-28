455d94c5e1e1c98621eaab16d13d17bd9d2c7079
n-tdi <79174456+n-tdi@users.noreply.github.com>
2022-12-28 15:27:27 -0400

    initial commit

---
+++ src/test/java/world/ntdi/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package world.ntdi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
